6df69ef8cea68db2a495df87ee2cd402d06bf04e
phxofc <phdfrocha99@gmail.com>
2024-04-22 22:52:20 -0300

    create controller

--- src/main/java/br/com/pedro/rankingfilmes/controllers/MovieController.java
+++ src/main/java/br/com/pedro/rankingfilmes/controllers/MovieController.java
@@ -1,10 +1,24 @@
 package br.com.pedro.rankingfilmes.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.pedro.rankingfilmes.models.Movie;
+import br.com.pedro.rankingfilmes.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
-@RequestMapping("/")
+@RequestMapping("/movie")
 public class MovieController {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @PostMapping("/create")
+    public ResponseEntity createMovie(@RequestBody Movie movie) {
+
+      var productSave =  this.movieRepository.save(movie);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productSave);
+    }
 
 }
