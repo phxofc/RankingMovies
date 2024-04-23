da817f9185ed1c41eb35ea7623b52e6e93d71404
phxofc <phdfrocha99@gmail.com>
2024-04-22 22:10:08 -0300

    create project

---
+++ src/main/java/br/com/pedro/rankingfilmes/RankingFilmesApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedro.rankingfilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankingFilmesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RankingFilmesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pedro/rankingfilmes/RankingFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.rankingfilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankingFilmesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
